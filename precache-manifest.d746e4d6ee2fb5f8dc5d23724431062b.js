70800b7324c88e7f6e1f7dfc0fa118f08e6a1358
Youssef Shaaban <youssef@gmail.com>
2020-08-14 09:50:44 +0200

    Updates

--- precache-manifest.d746e4d6ee2fb5f8dc5d23724431062b.js
+++ precache-manifest.d746e4d6ee2fb5f8dc5d23724431062b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a88f4d8fdbe1e639fe0f83b3c308e1c7",
+    "revision": "5db382de63ff2b0d8c1a65c38fcd63e0",
     "url": "/Todo-React-App/index.html"
   },
   {
-    "revision": "fb2915092ee02de2097c",
-    "url": "/Todo-React-App/static/css/main.40fc0a3a.chunk.css"
+    "revision": "35733d78bd00a085a5dc",
+    "url": "/Todo-React-App/static/css/main.3e359788.chunk.css"
   },
   {
     "revision": "22887f2f83b744822c2b",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Todo-React-App/static/js/2.d062322a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fb2915092ee02de2097c",
-    "url": "/Todo-React-App/static/js/main.5d3a13d4.chunk.js"
+    "revision": "35733d78bd00a085a5dc",
+    "url": "/Todo-React-App/static/js/main.2755f942.chunk.js"
   },
   {
     "revision": "ce33244c9586849e9e2a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Todo-React-App/precache-manifest.83f1dcd223c813ace136ef4d43dc8d1b.js"
+  "/Todo-React-App/precache-manifest.d746e4d6ee2fb5f8dc5d23724431062b.js"
 );
 
 self.addEventListener('message', (event) => {
