f16c9c2011f77f4da9e86f34418874977d30dd68
Youssef Shaaban <youssef@gmail.com>
2020-08-14 09:32:45 +0200

    Updates

---
+++ precache-manifest.19adfc0e73fe8346a03924bcbd909687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c863d0ce6c791de2544d603b0fdcaf7",
+    "url": "/Todo-React-App/index.html"
+  },
+  {
+    "revision": "a935a43b48426d8e50ab",
+    "url": "/Todo-React-App/static/css/main.e06b1faf.chunk.css"
+  },
+  {
+    "revision": "22887f2f83b744822c2b",
+    "url": "/Todo-React-App/static/js/2.d062322a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-React-App/static/js/2.d062322a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a935a43b48426d8e50ab",
+    "url": "/Todo-React-App/static/js/main.74f9e375.chunk.js"
+  },
+  {
+    "revision": "ce33244c9586849e9e2a",
+    "url": "/Todo-React-App/static/js/runtime-main.9f7a0fab.js"
+  }
+]);
